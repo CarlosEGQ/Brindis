7d5cedf8c6aa4976b6a55efea1d1a196b57493f6
Carlos <cxwataxc@gmail.com>
2020-02-18 06:00:50 -0600

    no ten chingando vergas.

--- Miniproyecto/Miniproyecto/Login.Designer.cs
+++ Miniproyecto/Miniproyecto/Login.Designer.cs
@@ -28,19 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.txtUserName = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.txtPassword = new System.Windows.Forms.TextBox();
             this.btnLogin = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial Narrow", 36F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(31, 19);
+            this.label1.Location = new System.Drawing.Point(59, 19);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(261, 57);
             this.label1.TabIndex = 0;
@@ -50,7 +55,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(24, 102);
+            this.label2.Location = new System.Drawing.Point(52, 102);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(145, 18);
             this.label2.TabIndex = 1;
@@ -59,7 +64,7 @@
             // txtUserName
             // 
             this.txtUserName.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtUserName.Location = new System.Drawing.Point(175, 99);
+            this.txtUserName.Location = new System.Drawing.Point(203, 99);
             this.txtUserName.Name = "txtUserName";
             this.txtUserName.Size = new System.Drawing.Size(127, 26);
             this.txtUserName.TabIndex = 2;
@@ -69,7 +74,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(24, 146);
+            this.label3.Location = new System.Drawing.Point(52, 146);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(89, 18);
             this.label3.TabIndex = 3;
@@ -78,7 +83,7 @@
             // txtPassword
             // 
             this.txtPassword.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtPassword.Location = new System.Drawing.Point(175, 143);
+            this.txtPassword.Location = new System.Drawing.Point(203, 143);
             this.txtPassword.Name = "txtPassword";
             this.txtPassword.PasswordChar = '*';
             this.txtPassword.Size = new System.Drawing.Size(127, 26);
@@ -87,7 +92,7 @@
             // btnLogin
             // 
             this.btnLogin.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnLogin.Location = new System.Drawing.Point(83, 195);
+            this.btnLogin.Location = new System.Drawing.Point(111, 195);
             this.btnLogin.Name = "btnLogin";
             this.btnLogin.Size = new System.Drawing.Size(136, 50);
             this.btnLogin.TabIndex = 5;
@@ -95,11 +100,33 @@
             this.btnLogin.UseVisualStyleBackColor = true;
             this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(13, 99);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(21, 25);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(13, 143);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(21, 25);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(314, 286);
+            this.ClientSize = new System.Drawing.Size(343, 286);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.btnLogin);
             this.Controls.Add(this.txtPassword);
             this.Controls.Add(this.label3);
@@ -110,6 +137,8 @@
             this.MinimizeBox = false;
             this.Name = "Login";
             this.Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -123,5 +152,7 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox txtPassword;
         private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
--- Miniproyecto/Miniproyecto/Visualizacion.Designer.cs
+++ Miniproyecto/Miniproyecto/Visualizacion.Designer.cs
@@ -28,83 +28,83 @@
         /// </summary>
         private void InitializeComponent()
         {
-			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Visualizacion));
-			this.pControl = new System.Windows.Forms.Panel();
-			this.toolStrip1 = new System.Windows.Forms.ToolStrip();
-			this.btnCategories = new System.Windows.Forms.ToolStripButton();
-			this.btnClients = new System.Windows.Forms.ToolStripButton();
-			this.btnUsers = new System.Windows.Forms.ToolStripButton();
-			this.toolStrip1.SuspendLayout();
-			this.SuspendLayout();
-			// 
-			// pControl
-			// 
-			this.pControl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Visualizacion));
+            this.pControl = new System.Windows.Forms.Panel();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.btnCategories = new System.Windows.Forms.ToolStripButton();
+            this.btnClients = new System.Windows.Forms.ToolStripButton();
+            this.btnUsers = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pControl
+            // 
+            this.pControl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-			this.pControl.Location = new System.Drawing.Point(1, 28);
-			this.pControl.Name = "pControl";
-			this.pControl.Size = new System.Drawing.Size(827, 454);
-			this.pControl.TabIndex = 0;
-			// 
-			// toolStrip1
-			// 
-			this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
-			this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pControl.Location = new System.Drawing.Point(1, 28);
+            this.pControl.Name = "pControl";
+            this.pControl.Size = new System.Drawing.Size(827, 454);
+            this.pControl.TabIndex = 0;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.btnCategories,
             this.btnClients,
             this.btnUsers});
-			this.toolStrip1.Location = new System.Drawing.Point(0, 0);
-			this.toolStrip1.Name = "toolStrip1";
-			this.toolStrip1.Size = new System.Drawing.Size(828, 27);
-			this.toolStrip1.TabIndex = 1;
-			this.toolStrip1.Text = "toolStrip1";
-			// 
-			// btnCategories
-			// 
-			this.btnCategories.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.btnCategories.Image = ((System.Drawing.Image)(resources.GetObject("btnCategories.Image")));
-			this.btnCategories.ImageTransparentColor = System.Drawing.Color.Magenta;
-			this.btnCategories.Name = "btnCategories";
-			this.btnCategories.Size = new System.Drawing.Size(194, 24);
-			this.btnCategories.Text = "Administrar Categorias";
-			this.btnCategories.Click += new System.EventHandler(this.btnCategories_Click_1);
-			// 
-			// btnClients
-			// 
-			this.btnClients.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.btnClients.Image = ((System.Drawing.Image)(resources.GetObject("btnClients.Image")));
-			this.btnClients.ImageTransparentColor = System.Drawing.Color.Magenta;
-			this.btnClients.Name = "btnClients";
-			this.btnClients.Size = new System.Drawing.Size(173, 24);
-			this.btnClients.Text = "Administrar Clientes";
-			this.btnClients.Click += new System.EventHandler(this.btnClients_Click);
-			// 
-			// btnUsers
-			// 
-			this.btnUsers.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.btnUsers.Image = ((System.Drawing.Image)(resources.GetObject("btnUsers.Image")));
-			this.btnUsers.ImageTransparentColor = System.Drawing.Color.Magenta;
-			this.btnUsers.Name = "btnUsers";
-			this.btnUsers.Size = new System.Drawing.Size(173, 24);
-			this.btnUsers.Text = "Administar Usuarios";
-			this.btnUsers.Click += new System.EventHandler(this.btnUsers_Click);
-			// 
-			// Visualizacion
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(828, 485);
-			this.Controls.Add(this.toolStrip1);
-			this.Controls.Add(this.pControl);
-			this.Name = "Visualizacion";
-			this.Text = "Visualizacion";
-			this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Visualizacion_FormClosed);
-			this.Load += new System.EventHandler(this.Visualizacion_Load);
-			this.toolStrip1.ResumeLayout(false);
-			this.toolStrip1.PerformLayout();
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(828, 27);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // btnCategories
+            // 
+            this.btnCategories.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCategories.Image = ((System.Drawing.Image)(resources.GetObject("btnCategories.Image")));
+            this.btnCategories.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnCategories.Name = "btnCategories";
+            this.btnCategories.Size = new System.Drawing.Size(194, 24);
+            this.btnCategories.Text = "Administrar Categorias";
+            this.btnCategories.Click += new System.EventHandler(this.btnCategories_Click_1);
+            // 
+            // btnClients
+            // 
+            this.btnClients.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClients.Image = ((System.Drawing.Image)(resources.GetObject("btnClients.Image")));
+            this.btnClients.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnClients.Name = "btnClients";
+            this.btnClients.Size = new System.Drawing.Size(173, 24);
+            this.btnClients.Text = "Administrar Clientes";
+            this.btnClients.Click += new System.EventHandler(this.btnClients_Click);
+            // 
+            // btnUsers
+            // 
+            this.btnUsers.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUsers.Image = ((System.Drawing.Image)(resources.GetObject("btnUsers.Image")));
+            this.btnUsers.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnUsers.Name = "btnUsers";
+            this.btnUsers.Size = new System.Drawing.Size(173, 24);
+            this.btnUsers.Text = "Administar Usuarios";
+            this.btnUsers.Click += new System.EventHandler(this.btnUsers_Click);
+            // 
+            // Visualizacion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(828, 485);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.pControl);
+            this.Name = "Visualizacion";
+            this.Text = "Visualizacion";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Visualizacion_FormClosed);
+            this.Load += new System.EventHandler(this.Visualizacion_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
